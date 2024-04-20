69228cf6abfdec11f515dcedf141a39de03be2b8
jameslol417 <jameslol4177@gmail.com>
2024-04-20 03:14:31 -0700

    gemini into reflex

--- gemini-test.py
+++ gemini-test.py
@@ -1,11 +1,8 @@
-from rxconfig import config
-import reflex as rx
 import google.generativeai as genai
 import json
 
-
 # Path to the JSON file containing the API key
-json_file_path = 'env.json'
+json_file_path = '.web/env.json'
 
 # Load the API key from the JSON file
 with open(json_file_path, 'r') as file:
@@ -17,22 +14,4 @@ genai.configure(api_key=google_api_key)
 
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("Give me python code to sort a list")
-
-
-class State(rx.State):
-    """The app state."""
-
-def index() -> rx.Component:
-    return rx.center(
-        rx.vstack(
-            rx.heading("Hello!!", size="9"),
-            align="center",
-            spacing="7",
-            font_size="2em",
-        ),
-        rx.text(response.text),
-        height="100vh",
-    )
-
-app = rx.App()
-app.add_page(index)
+print(response.text)
