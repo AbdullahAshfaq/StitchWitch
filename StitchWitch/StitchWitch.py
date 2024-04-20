778add1b64946a4eadb09bc0cc7d95a1b2398c88
jameslol417 <jameslol4177@gmail.com>
2024-04-20 03:23:31 -0700

    added safety filter for gemini

--- StitchWitch/StitchWitch.py
+++ StitchWitch/StitchWitch.py
@@ -15,6 +15,30 @@ with open(json_file_path, 'r') as file:
 # Configure the Gemini API with the loaded API key
 genai.configure(api_key=google_api_key)
 
+# Bypassing safety
+safe = [
+        {
+            "category": "HARM_CATEGORY_DANGEROUS",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE",
+        },
+    ]
+
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content("Give me python code to sort a list")
 
