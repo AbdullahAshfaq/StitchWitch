5b30fe78a6526acc7b798d47a720b3fe9ced0542
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2024-04-20 20:48:14 -0700

    embedded real time frontend streaming while parsing to backend

--- StitchWitch/StitchWitch.py
+++ StitchWitch/StitchWitch.py
@@ -1,8 +1,26 @@
-from rxconfig import config
 import reflex as rx
-# from .videocapture import capture_frames_from_video_async
+import cv2
+import os
+from time import sleep
+import asyncio
+from PIL import Image 
+from pathlib import Path
 
-video_exist = 0
+
+# Path to the JSON file containing the API key
+# json_file_path = 'env.json'
+
+# # Load the API key from the JSON file
+# with open(json_file_path, 'r') as file:
+#     config = json.load(file)
+#     google_api_key = config['GOOGLE_API_KEY']
+
+# Configure the Gemini API with the loaded API key
+# genai.configure(api_key=google_api_key)
+
+# model = genai.GenerativeModel('gemini-pro')
+# response = model.generate_content("Give me python code to sort a list")
+
 
 style = {
         "background-color": "#0C0E11",  # Dark background color
@@ -10,178 +28,103 @@ style = {
         "font-family": "sans-serif",  # Default font
         "margin": 0,  # Remove default margin
         "height": "100vh",  # Full viewport height
-        # "display": "flex",
-        "overflow": "hidden",
+        "display": "flex",
     }
+video_path = "sample-video.mp4" # Path to desired video
+cap = cv2.VideoCapture(0)
+output_folder = "captured_frames"  # Output folder to save captured frames
+interval_sec = 1  # Interval in seconds to capture frames
+# Get the frame rate of the video
+fps = cap.get(cv2.CAP_PROP_FPS)
+# Calculate frame interval based on desired interval_sec
+frame_interval = int(fps * interval_sec)
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
+
+output_path = Path("assets/frame.jpg")
 
 class State(rx.State):
     """The app state."""
-    videos: list[str] = []
-    video_exist: int = 0  # Manage video_exist as part of the state
+    frame_count = 0
+    output_path = "assets/frame.jpg"
+    
+    async def analyze_video(self):
+        while True:
+            # Read a frame from the video
+            ret, frame = cap.read()
+            print("nfaoifnoaenfoenofeao")
 
-    async def handle_upload(self, files: list[rx.UploadFile]):
-        """Handle the upload of video file(s)."""
-        for file in files:
-            upload_data = await file.read()
-            outfile = rx.get_upload_dir() / file.filename
-            with outfile.open("wb") as file_object:
-                file_object.write(upload_data)
-            self.videos.append(file.filename)
+            if not ret:
+                pass  # Break the loop if we've reached the end of the video
 
-        # Update video_exist based on the presence of videos
-        self.video_exist = 1 if self.videos else 0
+            # Display the current frame (video playback)
+            # cv2.imshow("Video Playback", frame)
+            output_path = f"assets/frames/frame{self.frame_count}.jpg"
+            print(output_path, frame)
+            cv2.imwrite(output_path, frame)
+            opencv_image = cv2.imread(output_path) 
+            frame_rgb = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+            pil_image = Image.fromarray(frame_rgb)
+            pil_image.save(output_path)
+            
+            self.output_path = output_path.removeprefix("assets/")
 
+            # Check if it's time to save a frame
+            if self.frame_count % frame_interval == 0:
+                # Save the frame as an image file
+                output_path = f"{output_folder}/frame_{self.frame_count // frame_interval}.jpg"
+                # cv2.imwrite(output_path, frame)
+                print(f"Saved frame {self.frame_count // frame_interval}")
 
-color = "#ab8bff"
+            # Increment frame counter
+            self.frame_count += 1
+            await asyncio.sleep(1/fps)
+            sleep(0.1)
+            output_path = f"assets/frames/frame{self.frame_count-1}.jpg"
+            # _, buffer = cv2.imencode('.jpg', frame)  # Encode frame as JPEG
+            # frame_bytes = base64.b64encode(buffer)  # Convert frame to base64
+            
+            yield
+            
+            # return frame_bytes
 
 
 def index() -> rx.Component:
-    if video_exist == 1:
-        return rx.vstack(
-            rx.hstack(
-                rx.image(src="/logo.png", width="260px", height="auto", margin_left="30px", margin_top="23px"),
-            ),
-            rx.hstack(
+    return rx.vstack(
+        rx.hstack(
+            rx.image(src="/logo.png", width="260px", height="auto", margin_left="30px", margin_top="23px"),
+        ),
+        rx.button(
+            "Start", on_click=State.analyze_video
+        ),
+        rx.hstack(
+            rx.vstack(
+                rx.heading("Live Procedure", margin_top="10px", margin_left="30px", margin_bottom="15px", font_size="30px"),
+                rx.image(src=State.output_path, width="520", height="auto", margin_left="30px", margin_top="23px"), # LIVE STREAM
                 rx.vstack(
-                    rx.heading("Live Procedure", margin_top="10px", margin_left="30px", margin_bottom="15px", font_size="30px"),
-                    rx.video(
-                        url="sample-video.mp4",
-                        height="63vh",
-                        width="112vh",
-                        margin_left="30px",
-                        border="3px solid green", 
-                    ),
-                    # rx.vstack(
-                    #     background_color="#222423",
-                    #     height="63vh",
-                    #     width="112vh",
-                    #     margin_left="30px",
-                    #     border="3px solid green", 
-                    # ),
+                    background_color="#222423",
+                    height="63vh",
+                    width="112vh",
+                    margin_left="30px",
+                    border="3px solid green", 
                 ),
-                rx.vstack(
-                    rx.heading("Caption",margin_left="15px", margin_top="10px", margin_bottom="15px", font_size="30px"),
-                    rx.vstack(
-                        background_color="#222423",
-                        margin_left="15px",
-                        width="55vh",
-                        height="75vh",
-                    ),
-                    flex="1",
-                )
-            ),
-            rx.heading("Warnings: ", margin_top="-90px", margin_left="30px", font_size="20px", color="orange"),
-            rx.heading("Dangers: ", margin_left="30px", margin_top="-10px", font_size="20px", color="red"),
-        )
-    else:
-        return rx.vstack(
-            rx.hstack(
-                rx.image(src="/logo.png", width="260px", height="auto", margin_left="30px", margin_top="23px"),
             ),
             rx.vstack(
-                rx.heading(
-                        "This is StitchWitch,",
-                        font_size="60px",
-                        # background_image= "linear-gradient(95deg, #D6D6ED 42.14%, #727280 63.21%)",
-                        style={
-                            "color": "transparent",
-                            "background-image": "linear-gradient(95deg, #D6D6ED 42.14%, #727280 63.21%)",
-                            "background-clip": "text",
-                            "-webkit-background-clip": "text",  # For WebKit browsers
-                            "-webkit-text-fill-color": "transparent",  # Necessary for webkit browsers
-                            "line-height": "1.2",  # Increased line height
-                            "padding-top": "20px",  # Add padding at the top
-                        },
-                        height="auto",
-                ),
-                rx.heading(
-                    "the modern AI surgery assistant",
-                    font_size="60px",
-                    style={
-                            "color": "transparent",
-                            "background-image": "linear-gradient(95deg, #D6D6ED 42.14%, #727280 63.21%)",
-                            "background-clip": "text",
-                            "-webkit-background-clip": "text",  # For WebKit browsers
-                            "-webkit-text-fill-color": "transparent",  # Necessary for webkit browsers
-                            "line-height": "1.2",  # Increased line height  # Add padding at the top
-                            "padding-bottom": "20px",  # Add padding at the bottom
-                        },
-                    margin_top="-18px"
-                ),
+                rx.heading("Caption",margin_left="15px", margin_top="10px", margin_bottom="15px", font_size="30px"),
                 rx.vstack(
-                    rx.center(
-                        rx.heading("Select the Surgery Type", size="5", margin_bottom="10px", margin_top="-20px"),
-                        width="100%",
-                    ),
-                    rx.center(
-                        rx.select(
-                            ['Lumbar Discectomy', 'Heart Transplant', 'Cataract Surgery'],
-                            placeholder="Surgery Type",
-                            label="Surgery Types",
-                            align_items="center",
-                        ),
-                        width="100%",
-                    ),
-                    rx.center(
-                        rx.upload(
-                            rx.vstack(
-                                rx.heading("Drag and drop Medical Procedure here or click to select", size="5", margin_bottom="10px"),
-                                rx.button("Select PDF", color=color, bg="white", border=f"1.5px solid {color}", font_weight="600"),
-                                align_items="center",  # Center align items horizontally within the vertical stack
-                                justify_content="center",  # Center content vertically within the container, if needed
-                            ),
-                            id="upload1",
-                            accept={"application": ["pdf"]},
-                            border=f"1px solid {color}",
-                            padding="2.5em",
-                            justify_content="center",
-                            margin_top="40px",
-                        ),
-                            width="100%",
-
-                    ),
-                    rx.center(
-                            rx.button(
-                            "Submit",
-                            border_radius="1em",
-                            box_shadow="rgba(151, 65, 252, 0.8) 0 15px 30px -10px",
-                            background_image="linear-gradient(144deg,#AF40FF,#5B42F3 50%,#00DDEB)",
-                            box_sizing="border-box",
-                            color="white",
-                            opacity=1,
-                            _hover={
-                                "opacity": 0.5,
-                            },
-                            width="100px"
-                        ),
-                        width="100%",
-                        margin_top="30px"
-                    ),
-                    
-                    
-                    
-                    rx.hstack(rx.foreach(rx.selected_files("upload1"), rx.text)),
-                    # rx.button(
-                    #     "Upload",
-                    #     on_click=State.handle_upload(rx.upload_files(upload_id="upload1")),
-                    # ),
-                    # rx.button(
-                    #     "Clear",
-                    #     on_click=rx.clear_selected_files("upload1"),
-                    # ),
-                    rx.foreach(State.videos, lambda video: rx.video(src=rx.get_upload_url(video), controls=True)),
-                    padding="2em",
-                    width="100%",
+                    background_color="#222423",
+                    margin_left="15px",
+                    width="55vh",
+                    height="75vh",
                 ),
-                margin_top="7vh",  # Center horizontally
-                align_items="center",
-                width="100%",
-                height="100%", 
-            ),
-            width="100%"
-           
-            
-        )
+                flex="1",
+            )
+        ),
+        rx.heading("Warnings: ", margin_top="-90px", margin_left="30px", font_size="20px", color="orange"),
+        rx.heading("Dangers: ", margin_left="30px", margin_top="-10px", font_size="20px", color="red"),
+        #rx.text(response.text)
+    )
 
 app = rx.App(style=style)
 app.add_page(index)
